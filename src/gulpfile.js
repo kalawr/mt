521cafbefddce682494016de60658a19a5452d23
Illia Kalaur <kallaur125@gmail.com>
2016-03-30 10:08:13 +0300

    dest/ renamed to public/

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -5,17 +5,17 @@ gulp.task('jade', function ()
 {
 	return gulp.src('jade/*.jade')
 		.pipe(jade())
-		.pipe(gulp.dest('../dist'));
+		.pipe(gulp.dest('../public'));
 });
 
 gulp.task('css', function ()
 {
 	return gulp.src('css/*.css')
-		.pipe(gulp.dest('../dist/css'));
+		.pipe(gulp.dest('../public/css'));
 });
 
 gulp.task('js', function ()
 {
 	return gulp.src('js/*.js')
-		.pipe(gulp.dest('../dist/js'));
+		.pipe(gulp.dest('../public/js'));
 });
