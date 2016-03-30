7e9e8334b5805c5b956711516b45b11bb9583324
Illia Kalaur <kallaur125@gmail.com>
2016-03-30 12:15:26 +0300

    Add tasks to mirror dependencies to public/; Add normalize.css

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 
+
+var cssLibs = [
+	'bower_components/normalize-css/normalize.css'
+];
+
+var jsLibs = [
+	'bower_components/jquery/dist/jquery.min.js'
+];
+
+
 gulp.task('jade', function ()
 {
 	return gulp.src('jade/*.jade')
 		.pipe(jade())
 		.pipe(gulp.dest('../public'));
 });
+
 
 gulp.task('css', function ()
 {
@@ -16,13 +27,32 @@ gulp.task('css', function ()
 
 gulp.task('css-watch', function ()
 {
-	gulp.watch('css/*.css', ['css']);
+	return gulp.watch('css/*.css', ['css']);
 });
+
+gulp.task('css-pick-libs', function ()
+{
+	return gulp.src(cssLibs)
+		.pipe(gulp.dest('../public/css/lib'));
+});
+
 
 gulp.task('js', function ()
 {
 	return gulp.src('js/*.js')
 		.pipe(gulp.dest('../public/js'));
 });
+
+gulp.task('js-watch', function ()
+{
+	return gulp.watch('js/*.js', ['js']);
+});
+
+gulp.task('js-pick-libs', function ()
+{
+	return gulp.src(jsLibs)
+		.pipe(gulp.dest('../public/js/lib'));
+});
+
 
 gulp.task('default', ['jade', 'css', 'js']);
