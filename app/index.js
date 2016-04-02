4705764fe99b2f96514b176f7f4e95c50f3b8468
Illia Kalaur <kallaur125@gmail.com>
2016-04-02 18:29:16 +0300

    Add language mapping and functions to build foreign URLs

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
 var request = require('request');
+
+var langs = new Map()
+	.set('en', 1)
+	.set('ru', 2)
+	.set('de', 3)
+	.set('fr', 4)
+	.set('es', 5)
+	.set('it', 23)
+	;
+
+var buildUrl = function (type)
+{
+	return function (query, langs, langsMap)
+	{
+		var lang1 = langsMap.get(langs.split('-')[0]);
+		var lang2 = langsMap.get(langs.split('-')[1]);
+
+		return `http://www.multitran.ru/c/${type}.exe?s=${query}&l1=${lang1}&l2=${lang2}`;
+	};
+};
+
+var buildResultUrl = buildUrl('m');
+var buildHintUrl   = buildUrl('ms');
